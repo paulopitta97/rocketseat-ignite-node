649e4908f1742e6a754009950b4ce409fe4164b9
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2024-06-22 20:23:37 -0300

    Aula 03 - Test Environment do Prisma

--- aula-03-api-solid-node/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ aula-03-api-solid-node/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,12 +1,39 @@
+import 'dotenv/config'
+import { execSync } from 'child_process'
+import { randomUUID } from 'crypto'
 import { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('Setup')
+    const schema = randomUUID()
+
+    const newURL = generateDatabaseURL(schema)
+    process.env.DATABASE_URL = newURL
+
+    execSync('npx prisma migrate deploy')
+
     return {
-      teardown() {
-        console.log('Teardown')
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
