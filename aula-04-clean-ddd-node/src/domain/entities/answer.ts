211ee9ac29804ea4b2f5dbbb2dde21e783d1a3f4
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2024-11-08 00:58:20 -0300

    Aula 04 - abstraindo criação de entidades

--- aula-04-clean-ddd-node/src/domain/entities/answer.ts
+++ aula-04-clean-ddd-node/src/domain/entities/answer.ts
@@ -1,5 +1,6 @@
 import { Entity } from "../core/entities/entity"
 import { UniqueEntityID } from "../core/entities/unique-entity-id"
+import { Optional } from "../core/types/optional"
 
 interface AnswerProps {
     content: string
@@ -13,6 +14,14 @@ export class Answer extends Entity<AnswerProps> {
 
     get content() {
         return this.props.content
+    }
+
+    static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityID) {
+        const answer = new Answer({
+            ...props,
+            createdAt: new Date(),
+        }, id)
+        return answer
     }
 
 }
