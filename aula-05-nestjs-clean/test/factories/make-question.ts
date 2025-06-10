0b73f686bfb26e332c912496b578c8a37570beec
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2025-06-09 23:04:29 -0300

    Aula 05 - utiliza factories nos testes e2e

--- aula-05-nestjs-clean/test/factories/make-question.ts
+++ aula-05-nestjs-clean/test/factories/make-question.ts
@@ -1,4 +1,8 @@
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaQuestionMapper } from '@/infra/database/prisma/mappers/prisma-question-mapper'
+
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
   Question,
@@ -19,4 +23,21 @@ export function makeQuestion(
     id,
   )
   return question
+}
+
+@Injectable()
+export class QuestionFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestion(
+    data: Partial<QuestionProps> = {},
+  ): Promise<Question> {
+    const question = makeQuestion(data)
+
+    await this.prisma.question.create({
+      data: PrismaQuestionMapper.toPrisma(question),
+    })
+
+    return question
+  }
 }
--- aula-05-nestjs-clean/test/factories/make-student.ts
+++ aula-05-nestjs-clean/test/factories/make-student.ts
@@ -1,4 +1,7 @@
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaStudentMapper } from '@/infra/database/prisma/mappers/prisma-student-mapper'
 
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
@@ -21,4 +24,19 @@ export function makeStudent(
   )
 
   return student
+}
+
+@Injectable()
+export class StudentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaStudent(data: Partial<StudentProps> = {}): Promise<Student> {
+    const student = makeStudent(data)
+
+    await this.prisma.user.create({
+      data: PrismaStudentMapper.toPrisma(student),
+    })
+
+    return student
+  }
 }
