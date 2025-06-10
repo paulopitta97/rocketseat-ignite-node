a4f5f5a529fc74d38dccea8ffc615637fae39fe9
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2025-06-09 22:53:36 -0300

    Aula 05 - implementa controller de buscar pergunta por slug

--- aula-05-nestjs-clean/src/infra/http/http.module.ts
+++ aula-05-nestjs-clean/src/infra/http/http.module.ts
@@ -10,6 +10,8 @@ import { DatabaseModule } from '../database/database.module';
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student';
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student';
 import { CryptographyModule } from '../cryptography/cryptography.module';
+import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller';
+import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug';
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -18,12 +20,14 @@ import { CryptographyModule } from '../cryptography/cryptography.module';
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
+    GetQuestionBySlugController
   ],
   providers: [
     CreateQuestionUseCase,
     FetchRecentQuestionsUseCase,
     RegisterStudentUseCase,
-    AuthenticateStudentUseCase
+    AuthenticateStudentUseCase,
+    GetQuestionBySlugUseCase
   ]
 })
 export class HttpModule {}
