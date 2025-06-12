862d1c271d31091ec1245b1b9a1a5dc462179f7e
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2025-06-11 23:05:28 -0300

    Aula 05 - implementa controller de editar pergunta

---
+++ aula-05-nestjs-clean/src/infra/http/controllers/edit-question.controller.e2e-spec.ts
@@ -0,0 +1,64 @@
+import { Test } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import request from 'supertest'
+import { StudentFactory } from 'test/factories/make-student'
+import { QuestionFactory } from 'test/factories/make-question'
+
+describe('Edit question (E2E)', () => {
+  let app: INestApplication
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let prisma: PrismaService
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    prisma = moduleRef.get(PrismaService)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[PUT] /questions/:id', async () => {
+    const user = await studentFactory.makePrismaStudent()
+
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+
+    const questionId = question.id.toString()
+
+    const response = await request(app.getHttpServer())
+      .put(`/questions/${questionId}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        title: 'New title',
+        content: 'New content',
+      })
+
+    expect(response.statusCode).toBe(204)
+
+    const questionOnDatabase = await prisma.question.findFirst({
+      where: {
+        title: 'New title',
+        content: 'New content',
+      },
+    })
+
+    expect(questionOnDatabase).toBeTruthy()
+  })
+})
--- aula-05-nestjs-clean/src/infra/http/controllers/fetch-recent-questions.controller.e2e-spec.ts
+++ aula-05-nestjs-clean/src/infra/http/controllers/fetch-recent-questions.controller.e2e-spec.ts
@@ -36,7 +36,7 @@ describe('Fetch recent questions (E2E)', () => {
     const accessToken = jwt.sign({ sub: user.id.toString() })
 
     await Promise.all([
-      questionFactory.makePrismaQuestion({ authorId: user.id, title: 'Question 01'}),
+      questionFactory.makePrismaQuestion({ authorId: user.id, title: 'Question 01' }),
       questionFactory.makePrismaQuestion({ authorId: user.id, title: 'Question 02' }),
     ])
 
@@ -47,10 +47,10 @@ describe('Fetch recent questions (E2E)', () => {
 
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual({
-      questions: [
+      questions: expect.arrayContaining([
         expect.objectContaining({ title: 'Question 01' }),
         expect.objectContaining({ title: 'Question 02' }),
-      ],
+      ]),
     })
   })
 })
