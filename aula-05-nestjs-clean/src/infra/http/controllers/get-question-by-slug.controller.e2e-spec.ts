fdb6df2b42dedc8f1beabd9556318cd0bbedbcab
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2025-06-11 22:51:08 -0300

    Aula 05 - refatora testes e2e e factories

--- aula-05-nestjs-clean/src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
+++ aula-05-nestjs-clean/src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 import { AppModule } from '@/infra/app.module'
 import { DatabaseModule } from '@/infra/database/database.module'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
+// import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { INestApplication } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
@@ -11,7 +11,7 @@ import { StudentFactory } from 'test/factories/make-student'
 
 describe('Get question by slug (E2E)', () => {
   let app: INestApplication
-  let prisma: PrismaService
+  // let prisma: PrismaService
   let studentFactory: StudentFactory
   let questionFactory: QuestionFactory
   let jwt: JwtService
@@ -23,7 +23,7 @@ describe('Get question by slug (E2E)', () => {
     }).compile()
 
     app = moduleRef.createNestApplication()
-    prisma = moduleRef.get(PrismaService)
+    // prisma = moduleRef.get(PrismaService)
     studentFactory = moduleRef.get(StudentFactory)
     questionFactory = moduleRef.get(QuestionFactory)
     jwt = moduleRef.get(JwtService)
