096c3d2168a2766a04b7555bd05812f7471f9aea
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2025-06-13 00:47:38 -0300

    Aula 05 - implementa controller de listar comentários da resposta

---
+++ aula-05-nestjs-clean/src/infra/http/controllers/fetch-answer-comments.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  BadRequestException,
+} from '@nestjs/common'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { z } from 'zod'
+import { FetchAnswerCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-answer-comments'
+import { CommentPresenter } from '../presenters/comment-presenter'
+
+// Validação da query de página
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+@Controller('/answers/:answerId/comments')
+export class FetchAnswerCommentsController {
+  constructor(private fetchAnswerComments: FetchAnswerCommentsUseCase) {}
+
+  @Get()
+  async handle(
+    @Query('page', queryValidationPipe) page: PageQueryParamSchema,
+    @Param('answerId') answerId: string,
+  ) {
+    const result = await this.fetchAnswerComments.execute({
+      page,
+      answerId,
+    })
+
+    if (result.isFailure()) {
+      throw new BadRequestException()
+    }
+
+    const answerComments = result.value.answerComments
+
+    return { comments: answerComments.map(CommentPresenter.toHTTP) }
+  }
+}
