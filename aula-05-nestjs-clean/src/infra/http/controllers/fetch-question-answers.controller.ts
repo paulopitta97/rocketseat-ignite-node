8a7660f3337dc9ac3c495fc70ec7192efc3ba847
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2025-06-13 00:00:39 -0300

    Aula 05 - implementa controller de listar respostas da pergunta

---
+++ aula-05-nestjs-clean/src/infra/http/controllers/fetch-question-answers.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  BadRequestException,
+} from '@nestjs/common'
+import { FetchQuestionAnswersUseCase } from '@/domain/forum/application/use-cases/fetch-question-answers'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { z } from 'zod'
+import { AnswerPresenter } from '../presenters/answer-presenter'
+
+// Validação da query de página
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+@Controller('/questions/:questionId/answers')
+export class FetchQuestionAnswersController {
+  constructor(
+    private fetchQuestionAnswers: FetchQuestionAnswersUseCase,
+  ) {}
+
+  @Get()
+  async handle(
+    @Query('page', queryValidationPipe) page: PageQueryParamSchema,
+    @Param('questionId') questionId: string,
+  ) {
+    const result = await this.fetchQuestionAnswers.execute({
+      page,
+      questionId,
+    })
+
+    if (result.isFailure()) {
+      throw new BadRequestException()
+    }
+
+    const answers = result.value.answers
+    return { answers: answers.map(AnswerPresenter.toHTTP) }
+  }
+}
