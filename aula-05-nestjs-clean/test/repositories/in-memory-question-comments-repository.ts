7ca70c77011d06504f74c60baf0959213eebada4
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2025-07-09 01:06:17 -0300

    Aula 05 - implementa listagem comentários com autor

--- aula-05-nestjs-clean/test/repositories/in-memory-question-comments-repository.ts
+++ aula-05-nestjs-clean/test/repositories/in-memory-question-comments-repository.ts
@@ -1,11 +1,15 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+import { InMemoryStudentsRepository } from './in-memory-students-repository'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-autor'
 
 export class InMemoryQuestionCommentsRepository
   implements QuestionCommentsRepository
 {
   public items: QuestionComment[] = []
+
+  constructor(private studentsRepository: InMemoryStudentsRepository) {}
 
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment)
@@ -32,5 +36,31 @@ export class InMemoryQuestionCommentsRepository
       .slice((page - 1) * 20, page * 20)
 
     return questionComments
+  }
+
+  async findManyByQuestionIdWithAuthor(questionId: string, { page }: PaginationParams) {
+    const comments = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+      .map((comment) => {
+        const author = this.studentsRepository.items.find((student) => {
+          return student.id.equals(comment.authorId)
+        })
+
+        if(!author) {
+          throw new Error(`Author with ID "${comment.authorId.toString()} does not exist`)
+        }
+
+        return CommentWithAuthor.create({
+          commentId: comment.id,
+          content: comment.content,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+          authorId: comment.authorId,
+          author: author.name,
+        })
+      })
+
+    return comments
   }
 }
