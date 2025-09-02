ec6cb68e70f45ea6006a9e4815c575af7b238b11
Paulo Eduardo <paulopitta97@gmail.com>
2025-09-01 22:19:50 -0300

    Aula 05 - implementa controller de leitura de notificação

--- aula-05-nestjs-clean/src/infra/http/http.module.ts
+++ aula-05-nestjs-clean/src/infra/http/http.module.ts
@@ -41,6 +41,8 @@ import { FetchAnswerCommentsController } from './controllers/fetch-answer-commen
 import { UploadAttachmentController } from './controllers/upload-attachment.controller'
 import { UploadAndCreateAttachmentUseCase } from '@/domain/forum/application/use-cases/upload-and-create-attachment'
 import { StorageModule } from '../storage/storage.module'
+import { ReadNotificationController } from './controllers/read-notification.controller'
+import { ReadNotificationUseCase } from '@/domain/notification/application/use-cases/read-notification'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule, StorageModule],
@@ -64,6 +66,7 @@ import { StorageModule } from '../storage/storage.module'
     FetchQuestionCommentsController,
     FetchAnswerCommentsController,
     UploadAttachmentController,
+    ReadNotificationController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -85,6 +88,7 @@ import { StorageModule } from '../storage/storage.module'
     FetchQuestionCommentsUseCase,
     FetchAnswerCommentsUseCase,
     UploadAndCreateAttachmentUseCase,
+    ReadNotificationUseCase,
   ],
 })
 export class HttpModule {}
