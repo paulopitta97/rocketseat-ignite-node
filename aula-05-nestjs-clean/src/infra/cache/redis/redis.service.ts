4779e1ee5222d7703cfbc96af55e30ea4ce246d9
Paulo Eduardo <paulopitta97@gmail.com>
2025-09-02 00:03:01 -0300

    Aula 05 - ajustes no cast de variáveis env

--- aula-05-nestjs-clean/src/infra/cache/redis/redis.service.ts
+++ aula-05-nestjs-clean/src/infra/cache/redis/redis.service.ts
@@ -7,8 +7,8 @@ export class RedisService extends Redis implements OnModuleDestroy {
   constructor(envService: EnvService) {
     super({
       host: envService.get('REDIS_HOST'),
-      port: Number(envService.get('REDIS_PORT')),
-      db: Number(envService.get('REDIS_DB')),
+      port: envService.get('REDIS_PORT'),
+      db: envService.get('REDIS_DB'),
     })
   }
 
